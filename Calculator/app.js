7cff1d8c85eda6c9e6f22b6489dea7383f97609b
atreat738 <atreat738@gmail.com>
2021-06-19 10:34:12 -0500

    Added more styling

--- Calculator/app.js
+++ Calculator/app.js
@@ -14,8 +14,16 @@ function divide(num1, num2) {
     num1 / num2;
 }
 
-// function operate(num1, operator, num2) {
-    
-// }
+function operate(num1, operator, num2) {
+    if (operator == add) {
+        add(num1, num2);
+    } else if (operator == subtract) {
+        subtract(num1, num2);
+    } else if (operator == multiply) {
+        multiply(num1, num2);
+    } else if (operator == divide) {
+        divide(num1, num2);
+    }
+}   
 
 //operate must call the operator and 2 numbers
