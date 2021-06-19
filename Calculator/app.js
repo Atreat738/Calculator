1fb49ce396092a0910339c0dc6d8b0bca8debd61
atreat738 <atreat738@gmail.com>
2021-06-19 10:00:48 -0500

    Added a basic HTML skeleton and a little CSS

---
+++ Calculator/app.js
@@ -0,0 +1,21 @@
+function add (num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    num1 * num2;
+}
+
+function divide(num1, num2) {
+    num1 / num2;
+}
+
+// function operate(num1, operator, num2) {
+    
+// }
+
+//operate must call the operator and 2 numbers
---
+++ README.md
@@ -0,0 +1,1 @@
+This is a project I am working on to sharpen my skills. 
