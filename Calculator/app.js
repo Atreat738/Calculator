f56cf10efd7d8cfd071f3d26dd64d1b334a933f6
atreat738 <atreat738@gmail.com>
2021-06-22 12:00:49 -0500

    Added functionality to the delete button. Can also now chain numbers together to make multi-digit numbers

--- Calculator/app.js
+++ Calculator/app.js
@@ -2,11 +2,13 @@
 const container = document.querySelector('#container');
 const clearButton = document.querySelector('#clearButton');
 const deleteButton = document.querySelector('#deleteButton');
+const operatorButton = document.querySelector('.operatorButton');
 const buttonDivide = document.querySelector('#buttonDivide');
 const buttonMultiply = document.querySelector('#buttonMultiply');
 const buttonSubtract = document.querySelector('#buttonSubtract');
 const buttonAddition = document.querySelector('#buttonAddition');
 const buttonDecimal = document.querySelector('#buttonDecimal');
+const numberButton = document.querySelector('.numberButton');
 const button0 = document.querySelector('#button0');
 const button1 = document.querySelector('#button1');
 const button2 = document.querySelector('#button2');
@@ -18,7 +20,7 @@ const button7 = document.querySelector('#button7');
 const button8 = document.querySelector('#button8');
 const button9 = document.querySelector('#button9');
 const buttonEquals = document.querySelector('#buttonEquals');
-const textDisplayContainer = document.querySelector('#textDisplayContainer');
+const textDisplayContainer = document.querySelector('#textboxContainer');
 const textDisplay = document.querySelector('#textDisplay p');
 
 //The functions below will define the operator functions
@@ -39,9 +41,10 @@ function divide(num1, num2) {
     num1 / num2;
 }
 
+
 function operate(num1, operator, num2) {
     if (operator == add) {
-        return add(num1, num2);
+        textDisplay.textContent = `${num1} + ${num2}`;
     } else if (operator == subtract) {
         return subtract(num1, num2);
     } else if (operator == multiply) {
@@ -51,28 +54,39 @@ function operate(num1, operator, num2) {
     }
 }   
 
-container.addEventListener('click', clickButton);
-
 //function needs to call the correct button value
 //must save the value into a variable to use later
 //must change textbox value to display the correct button value
 
+container.addEventListener('click', clickButton);
+
 function clickButton(e) {
-    if (e.target !== e.currentTarget && e.target !== e.textDisplayContainer) {
-        if (e.target == e.clearButton) {
-                let clickedItem = e.target.textContent;
+    if (e.target !== e.currentTarget && e.target !== textDisplayContainer) {
+        let numText = textDisplay.textContent;
+        if (e.target == clearButton) {
                 textDisplay.textContent = 0;
-            } else if (e.target == e.deleteButton) {
-                //Change this later
-                let clickedItem = e.target.textContent;
-                textDisplay.textContent = 0;
-            } else {
-        let clickedItem = e.target.textContent;
-        textDisplay.textContent = clickedItem;
+            } else if (e.target == deleteButton) {
+                let newStr = numText.slice(0, -1);
+                textDisplay.textContent = newStr;
+                if (newStr == '') {
+                    textDisplay.textContent = 0;
+                } 
+                return num1 = parseInt(numText);               
+            } else {                   
+                    if (textDisplay.textContent == 0 && e.targert !== buttonDecimal) {
+                        return textDisplay.textContent = e.target.textContent;
+                    }
+                    textDisplay.textContent += e.target.textContent;
+                    numText = e.target.textContent;
+                    return num1 = parseInt(e.target.textContent);                   
             }
     }
-
 }
-//delete and clear will be fixed next update
 
-//delete button should delete the last item that was entered.
+//input num1, 
+//if the beginning number is zero and no operator is pressed, the first input number should replace 0. 
+//and then an operator, 
+//once the operator is called, the next input will be num2.
+//and then = should return the final result.
+
+//let equation = {num1: num1, operator: operator, num2: num2};
