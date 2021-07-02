54c67d01ca58b74b3d6dfba8675b4f8769ffe5de
atreat738 <atreat738@gmail.com>
2021-07-02 17:26:09 -0500

    Cleaned and condensed the code to make it easier to read.

--- Calculator/app.js
+++ Calculator/app.js
@@ -1,24 +1,9 @@
 //This first section declares every textbox and button in javascript
-const container = document.querySelector('#container');
 const clearButton = document.querySelector('#clearButton');
 const deleteButton = document.querySelector('#deleteButton');
 const operators = document.querySelectorAll('.operatorButton');
-const buttonDivide = document.querySelector('#buttonDivide');
-const buttonMultiply = document.querySelector('#buttonMultiply');
-const buttonSubtract = document.querySelector('#buttonSubtract');
-const buttonAddition = document.querySelector('#buttonAddition');
 const buttonDecimal = document.querySelector('#buttonDecimal');
 const numberButtons = document.querySelectorAll('.numberButton');
-const button0 = document.querySelector('#button0');
-const button1 = document.querySelector('#button1');
-const button2 = document.querySelector('#button2');
-const button3 = document.querySelector('#button3');
-const button4 = document.querySelector('#button4');
-const button5 = document.querySelector('#button5');
-const button6 = document.querySelector('#button6');
-const button7 = document.querySelector('#button7');
-const button8 = document.querySelector('#button8');
-const button9 = document.querySelector('#button9');
 const buttonEquals = document.querySelector('#buttonEquals');
 const textDisplayContainer = document.querySelector('#textboxContainer');
 const textDisplay = document.querySelector('#textDisplay p');
@@ -27,8 +12,8 @@ let num1 = null;
 let currentOperator = null;
 let num2 = null;
 let result = null;
-//The functions below will define the operator functions
 
+//The functions below will define the operator functions
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -49,11 +34,6 @@ function divide(num1, num2) {
 }
 
 
-function getDisplayValue() {
-    return textDisplay.textContent;
-}
-//let equation = {firstNum: num1, operator: operator, secondNum: num2};
-
 function operate(num1, operator, num2) {
     if (operator == add) {
         return add(num1, num2);
@@ -66,9 +46,25 @@ function operate(num1, operator, num2) {
     }
 }   
 
-//function needs to call the correct button value
-//must save the value into a variable to use later
-//must change textbox value to display the correct button value
+
+function getDisplayValue() {
+    return textDisplay.textContent;
+}
+
+function setOperator(operator) {
+    if(currentOperator == null) {
+        currentOperator = operator;
+    }
+}
+
+function setNum(value) {
+    if(num1 == null) {
+    num1 = value;
+    } else {
+    num2 = value;
+    }
+}
+
 
 numberButtons.forEach((numberButton) => {
     numberButton.addEventListener('click', (e) => {
@@ -81,7 +77,9 @@ numberButtons.forEach((numberButton) => {
 
 operators.forEach((operator) => {
     operator.addEventListener('click', (e) => {
+        setNum(getDisplayValue());
         textDisplay.textContent = e.target.textContent;
+        setOperator(e.target);        
     })
 });
 
@@ -92,15 +90,10 @@ deleteButton.addEventListener('click', (e) => {
 });
 
 clearButton.addEventListener('click', () =>{
+    num1 = null;
+    currentOperator = null;
+    num2 = null;
     textDisplay.textContent = 0;
 });
-//input num1, 
-//if the beginning number is zero and no operator is pressed, the first input number should replace 0. 
-//and then an operator, 
-//once the operator is called, the next input will be num2.
-//and then = should return the final result. = will also trigger the math functions/operate
-
 
 // equation.firstNum = parseInt(textDisplay.textContent); 
-// console.log(equation.firstNum);   
-// console.log(e.target);
