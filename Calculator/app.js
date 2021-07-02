83bd306d03e60d7eedfde6666b259cb08ba1b430
atreat738 <atreat738@gmail.com>
2021-07-02 16:32:22 -0500

    Broke up the big addEventListener into multiple addEventListener and functions

--- Calculator/app.js
+++ Calculator/app.js
@@ -2,13 +2,13 @@
 const container = document.querySelector('#container');
 const clearButton = document.querySelector('#clearButton');
 const deleteButton = document.querySelector('#deleteButton');
-const operatorButton = document.querySelector('.operatorButton');
+const operators = document.querySelectorAll('.operatorButton');
 const buttonDivide = document.querySelector('#buttonDivide');
 const buttonMultiply = document.querySelector('#buttonMultiply');
 const buttonSubtract = document.querySelector('#buttonSubtract');
 const buttonAddition = document.querySelector('#buttonAddition');
 const buttonDecimal = document.querySelector('#buttonDecimal');
-const numberButton = document.querySelector('.numberButton');
+const numberButtons = document.querySelectorAll('.numberButton');
 const button0 = document.querySelector('#button0');
 const button1 = document.querySelector('#button1');
 const button2 = document.querySelector('#button2');
@@ -23,6 +23,10 @@ const buttonEquals = document.querySelector('#buttonEquals');
 const textDisplayContainer = document.querySelector('#textboxContainer');
 const textDisplay = document.querySelector('#textDisplay p');
 
+let num1 = null;
+let currentOperator = null;
+let num2 = null;
+let result = null;
 //The functions below will define the operator functions
 
 function add(num1, num2) {
@@ -34,17 +38,25 @@ function subtract(num1, num2) {
 }
 
 function multiply(num1, num2) {
-    num1 * num2;
+    return num1 * num2;
 }
 
 function divide(num1, num2) {
-    num1 / num2;
+    if (num2 == 0) {
+        alert('I don\'t want to divide by zero!');
+        textDisplay.textcontent = 0;
+    } else { return num1 / num2; }
 }
 
 
+function getDisplayValue() {
+    return textDisplay.textContent;
+}
+//let equation = {firstNum: num1, operator: operator, secondNum: num2};
+
 function operate(num1, operator, num2) {
     if (operator == add) {
-        textDisplay.textContent = `${num1} + ${num2}`;
+        return add(num1, num2);
     } else if (operator == subtract) {
         return subtract(num1, num2);
     } else if (operator == multiply) {
@@ -58,35 +70,37 @@ function operate(num1, operator, num2) {
 //must save the value into a variable to use later
 //must change textbox value to display the correct button value
 
-container.addEventListener('click', clickButton);
+numberButtons.forEach((numberButton) => {
+    numberButton.addEventListener('click', (e) => {
+        if (textDisplay.textContent == 0 && e.target !== buttonDecimal) {
+            return textDisplay.textContent = e.target.textContent;
+        }
+        textDisplay.textContent += e.target.textContent;       
+    })
+});
 
-function clickButton(e) {
-    if (e.target !== e.currentTarget && e.target !== textDisplayContainer) {
-        let numText = textDisplay.textContent;
-        if (e.target == clearButton) {
-                textDisplay.textContent = 0;
-            } else if (e.target == deleteButton) {
-                let newStr = numText.slice(0, -1);
-                textDisplay.textContent = newStr;
-                if (newStr == '') {
-                    textDisplay.textContent = 0;
-                } 
-                return num1 = parseInt(numText);               
-            } else {                   
-                    if (textDisplay.textContent == 0 && e.targert !== buttonDecimal) {
-                        return textDisplay.textContent = e.target.textContent;
-                    }
-                    textDisplay.textContent += e.target.textContent;
-                    numText = e.target.textContent;
-                    return num1 = parseInt(e.target.textContent);                   
-            }
-    }
-}
+operators.forEach((operator) => {
+    operator.addEventListener('click', (e) => {
+        textDisplay.textContent = e.target.textContent;
+    })
+});
 
+deleteButton.addEventListener('click', (e) => {
+    let newStr = textDisplay.textContent.slice(0, -1);
+    textDisplay.textContent = newStr;
+    if (newStr == '') {textDisplay.textContent = 0;}
+});
+
+clearButton.addEventListener('click', () =>{
+    textDisplay.textContent = 0;
+});
 //input num1, 
 //if the beginning number is zero and no operator is pressed, the first input number should replace 0. 
 //and then an operator, 
 //once the operator is called, the next input will be num2.
-//and then = should return the final result.
+//and then = should return the final result. = will also trigger the math functions/operate
 
-//let equation = {num1: num1, operator: operator, num2: num2};
+
+// equation.firstNum = parseInt(textDisplay.textContent); 
+// console.log(equation.firstNum);   
+// console.log(e.target);
