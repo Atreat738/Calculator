9943f437fa5f1a54c06049bb033a706e8b4da242
atreat738 <atreat738@gmail.com>
2021-08-04 15:33:53 -0500

    Debugged display issues

--- Calculator/app.js
+++ Calculator/app.js
@@ -11,41 +11,46 @@ const textDisplay = document.querySelector('#textDisplay p');
 let num1 = null;
 let currentOperator = null;
 let num2 = null;
+let toBeCleaned = false;
 let result = null;
 
 //The functions below will define the operator functions
-function add(num1, num2) {
-    return num1 + num2;
+function add(a, b) {
+    return a + b;
 }
 
-function subtract(num1, num2) {
-    return num1 - num2;
+function subtract(a, b) {
+    return a - b;
 }
 
-function multiply(num1, num2) {
-    return num1 * num2;
+function multiply(a, b) {
+    return a * b;
 }
 
-function divide(num1, num2) {
-    if (num2 == 0) {
+function divide(a, b) {
+    if (b == 0) {
         alert('I don\'t want to divide by zero!');
         textDisplay.textcontent = 0;
-    } else { return num1 / num2; }
+    } else { return a / b; }
 }
 
 
-function operate(num1, operator, num2) {
-    if (operator == add) {
-        return add(num1, num2);
-    } else if (operator == subtract) {
-        return subtract(num1, num2);
-    } else if (operator == multiply) {
-        return multiply(num1, num2);
-    } else if (operator == divide) {
-        return divide(num1, num2);
+function operate(a, b, currentOperator) {
+    if (currentOperator == add) {
+        return add(a, b);
+    } else if (currentOperator == subtract) {
+        return subtract(a, b);
+    } else if (currentOperator == multiply) {
+        return multiply(a, b);
+    } else if (currentOperator == divide) {
+        return divide(a, b);
     }
 }   
 
+//displayValue is used to chain numbers together
+function displayValue(value) {
+    textDisplay.textContent = textDisplay.textContent + value;
+}
 
 function getDisplayValue() {
     return textDisplay.textContent;
@@ -53,15 +58,33 @@ function getDisplayValue() {
 
 function setOperator(operator) {
     if(currentOperator == null) {
-        currentOperator = operator;
+        return currentOperator = operator;
+    } else if(num1 && num2) {
+        result = operate(Number(num1), Number(num2), currentOperator);
+        textDisplay.textContent = "";
+        displayValue(result);
+        num1 = result;
+        num2 = null;
+        return currentOperator = operator;
     }
 }
 
 function setNum(value) {
-    if(num1 == null) {
-    num1 = value;
+    if(num1 == null) {     
+        return num1 = getDisplayValue();
+    } else if (num2 == null && num1 != null) {
+        console.log(num1);
+        console.log(currentOperator);
+        return num2 = getDisplayValue();
+    }
+}
+
+function createResult() {
+    if(num1 && currentOperator && !toBeCleaned && !num2) {
+        setNum(getDisplayValue());
+        return operate(Number(num1), Number(num2), currentOperator);
     } else {
-    num2 = value;
+        return false;
     }
 }
 
@@ -71,29 +94,41 @@ numberButtons.forEach((numberButton) => {
         if (textDisplay.textContent == 0 && e.target !== buttonDecimal) {
             return textDisplay.textContent = e.target.textContent;
         }
-        textDisplay.textContent += e.target.textContent;       
+        if(toBeCleaned == true) {
+            textDisplay.textContent = "";
+        }
+        toBeCleaned = false;
+        displayValue(e.target.textContent);               
     })
-});
+})
 
 operators.forEach((operator) => {
     operator.addEventListener('click', (e) => {
-        setNum(getDisplayValue());
+        setNum();
         textDisplay.textContent = e.target.textContent;
-        setOperator(e.target);        
+        setOperator(e.target.id);  
+        toBeCleaned = true;      
     })
-});
+})
 
-deleteButton.addEventListener('click', (e) => {
+deleteButton.addEventListener('click', () => {
     let newStr = textDisplay.textContent.slice(0, -1);
     textDisplay.textContent = newStr;
     if (newStr == '') {textDisplay.textContent = 0;}
-});
+})
 
 clearButton.addEventListener('click', () =>{
     num1 = null;
     currentOperator = null;
     num2 = null;
     textDisplay.textContent = 0;
-});
+})
 
-// equation.firstNum = parseInt(textDisplay.textContent); 
+buttonEquals.addEventListener('click', () => {
+    result = createResult();
+    textDisplay.textContent = "";
+    displayValue(result);
+    console.log(num2);
+    console.log(result);
+})
+
